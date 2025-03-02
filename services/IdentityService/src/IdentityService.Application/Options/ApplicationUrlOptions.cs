abbd27edb707fa3c5a3960da25b063fb338be576
Gambitier <akash.jadhav.cse@gmail.com>
2025-03-02 16:20:49 +0530

    fix: email confirmation link generation and endpoint method

--- services/IdentityService/src/IdentityService.Application/Options/ApplicationUrlOptions.cs
+++ services/IdentityService/src/IdentityService.Application/Options/ApplicationUrlOptions.cs
@@ -7,4 +7,7 @@ public class ApplicationUrlOptions
     [Required(ErrorMessage = "BaseUrl is required")]
     [Url(ErrorMessage = "BaseUrl must be a valid URL")]
     public required string BaseUrl { get; init; }
+
+    [Required(ErrorMessage = "EmailConfirmationPath is required")]
+    public required string EmailConfirmationPath { get; init; }
 }
