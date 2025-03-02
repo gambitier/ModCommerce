efe73e221ffa1c75cb1dfb62aedcc4166f506bd3
Gambitier <akash.jadhav.cse@gmail.com>
2025-03-02 23:48:38 +0530

    chore: remove unused code

--- services/IdentityService/src/IdentityService.Infrastructure/Authentication/Options/EmailConfirmationTokenProviderOptions.cs
+++ services/IdentityService/src/IdentityService.Infrastructure/Authentication/Options/EmailConfirmationTokenProviderOptions.cs
@@ -2,11 +2,4 @@ using Microsoft.AspNetCore.Identity;
 
 namespace IdentityService.Infrastructure.Authentication.Options;
 
-public class EmailConfirmationTokenProviderOptions : DataProtectionTokenProviderOptions
-{
-    public EmailConfirmationTokenProviderOptions()
-    {
-        Name = "EmailDataProtectorTokenProvider";
-        TokenLifespan = TimeSpan.FromHours(2);
-    }
-}
+public class EmailConfirmationTokenProviderOptions : DataProtectionTokenProviderOptions { }
