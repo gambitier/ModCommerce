da587ae2ad1d1eb87278b16477ab63ac80b3f022
Gambitier <akash.jadhav.cse@gmail.com>
2025-03-16 15:44:00 +0530

    fix: ArgumentException - Value should not contain the default prefix 'urn:message:'. (Parameter 'urn')

--- services/IdentityService/src/IdentityService.Contracts/Constants/EventConstants.cs
+++ services/IdentityService/src/IdentityService.Contracts/Constants/EventConstants.cs
@@ -6,6 +6,6 @@ public static class EventConstants
     {
         private const string Prefix = "IdentityService.Exchanges.Users";
         public const string Exchange = $"{Prefix}:UserCreatedExchange";
-        public const string Urn = $"urn:message:ModCommerce:Events:UserCreated:v1";
+        public const string Urn = $"ModCommerce:Events:UserCreated:v1";
     }
 }
