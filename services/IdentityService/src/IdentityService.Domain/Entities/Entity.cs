43e32b3164667499180b1020d3d22ccfa8e8854d
Gambitier <akash.jadhav.cse@gmail.com>
2025-03-29 21:59:59 +0530

    docs: added summary docs for Events contracts

--- services/IdentityService/src/IdentityService.Domain/Entities/Entity.cs
+++ services/IdentityService/src/IdentityService.Domain/Entities/Entity.cs
@@ -2,17 +2,25 @@ using IdentityService.Domain.Interfaces.Events;
 
 namespace IdentityService.Domain.Entities;
 
+/// <summary>
+/// Base class for all entities.
+/// Use this class to add domain events to an entity.
+/// If it's not possible to inherit from this class, use the <see cref="IHasDomainEvents"/> interface instead.
+/// </summary>
 public abstract class Entity : IHasDomainEvents
 {
     private readonly List<object> _domainEvents = new();
 
+    /// <inheritdoc />
     public IReadOnlyCollection<object> DomainEvents => _domainEvents.AsReadOnly();
 
-    protected void AddDomainEvent(object domainEvent)
+    /// <inheritdoc />
+    public void AddDomainEvent(object domainEvent)
     {
         _domainEvents.Add(domainEvent);
     }
 
+    /// <inheritdoc />
     public void ClearDomainEvents()
     {
         _domainEvents.Clear();
--- services/IdentityService/src/IdentityService.Domain/Interfaces/Events/IHasDomainEvents.cs
+++ services/IdentityService/src/IdentityService.Domain/Interfaces/Events/IHasDomainEvents.cs
@@ -1,8 +1,24 @@
 namespace IdentityService.Domain.Interfaces.Events;
 
+/// <summary>
+/// Use this interface to add domain events to an entity.
+/// If it's not possible to inherit from the <see cref="Entities.Entity"/> class, use this interface instead.
+/// </summary>
 public interface IHasDomainEvents
 {
+    /// <summary>
+    /// Gets the domain events that have been added to the entity.
+    /// </summary>
     IReadOnlyCollection<object> DomainEvents { get; }
 
+    /// <summary>
+    /// Adds a domain event to the entity.
+    /// </summary>
+    /// <param name="domainEvent">The domain event to add.</param>
+    void AddDomainEvent(object domainEvent);
+
+    /// <summary>
+    /// Clears the domain events that have been added to the entity.
+    /// </summary>
     void ClearDomainEvents();
 }
