5de49251e90c2e3db2a768455a68c98cee261b29
Gambitier <akash.jadhav.cse@gmail.com>
2025-03-29 06:13:35 +0530

    feat: added migration to drop UserOrganizationMemberships table and create OrganizationMemberInvitations table.

---
+++ services/AccountService/src/AccountService.Infrastructure/Persistence/Migrations/20250329004019_ReplaceUserOrganizationMembershipsWithInvitations.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AccountService.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReplaceUserOrganizationMembershipsWithInvitations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserOrganizationMemberships");
+
+            migrationBuilder.CreateTable(
+                name: "OrganizationMemberInvitations",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    OrganizationId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Role = table.Column<int>(type: "integer", nullable: false),
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    InvitedByUserId = table.Column<string>(type: "text", nullable: false),
+                    AcceptedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    RejectedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ExpiresAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrganizationMemberInvitations", x => new { x.UserId, x.OrganizationId, x.Role });
+                    table.ForeignKey(
+                        name: "FK_OrganizationMemberInvitations_Organizations_OrganizationId",
+                        column: x => x.OrganizationId,
+                        principalTable: "Organizations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrganizationMemberInvitations_UserProfiles_UserId",
+                        column: x => x.UserId,
+                        principalTable: "UserProfiles",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrganizationMemberInvitations_OrganizationId",
+                table: "OrganizationMemberInvitations",
+                column: "OrganizationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrganizationMemberInvitations_UserId_OrganizationId_Role",
+                table: "OrganizationMemberInvitations",
+                columns: new[] { "UserId", "OrganizationId", "Role" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrganizationMemberInvitations");
+
+            migrationBuilder.CreateTable(
+                name: "UserOrganizationMemberships",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    OrganizationId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Role = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserOrganizationMemberships", x => new { x.UserId, x.OrganizationId, x.Role });
+                    table.ForeignKey(
+                        name: "FK_UserOrganizationMemberships_Organizations_OrganizationId",
+                        column: x => x.OrganizationId,
+                        principalTable: "Organizations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserOrganizationMemberships_UserProfiles_UserId",
+                        column: x => x.UserId,
+                        principalTable: "UserProfiles",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserOrganizationMemberships_OrganizationId",
+                table: "UserOrganizationMemberships",
+                column: "OrganizationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserOrganizationMemberships_UserId_OrganizationId_Role",
+                table: "UserOrganizationMemberships",
+                columns: new[] { "UserId", "OrganizationId", "Role" },
+                unique: true);
+        }
+    }
+}
